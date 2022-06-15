dbb71ca48c1a8794cc97773522b7de44274b6e72
LauraMoriones <104588455+LauraMoriones@users.noreply.github.com>
2022-06-15 14:03:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 //Lista de contactos Laura Moriones/
 
-const contactos = [
-  { id: 1, nombre: "Geralding Moriones", numero: 3124021228, tipo: "FM" },
-  { id: 2, nombre: "Yesid Vargas", numero: 3223060602, tipo: "FM" },
-  { id: 3, nombre: "Daniela Mayorga", numero: 3002316114, tipo: "EM" },
-  { id: 4, nombre: "Diana Avila", numero: 3123243164, tipo: "FM" },
-  { id: 5, nombre: "Angie Suarez", numero: 3212585010, tipo: "EM" },
-  { id: 6, nombre: "Natalia Acero", numero: 3227401539, tipo: "EM" },
-  { id: 7, nombre: "Martha Cardona", numero: 3006262243, tipo: "FM" },
-  { id: 8, nombre: "Andres Catañeda", numero: 3144341428, tipo: "EM" },
-  { id: 9, nombre: 3192837931, tipo: "EM" },
+const contactlist = [
+  { id: 1, name: "Geralding Moriones", number: 3124021228 },
+  { id: 2, name: "Yesid Vargas", number: 3223060602 },
+  { id: 3, name: "Daniela Mayorga", number: 3002316114 },
+  { id: 4, name: "Diana Avila", number: 3123243164 },
+  { id: 5, name: "Angie Suarez", number: 3212585010 },
+  { id: 6, name: "Natalia Acero", number: 3227401539 },
+  { id: 7, name: "Martha Cardona", number: 3006262243 },
+  { id: 8, name: "Andres Catañeda", number: 3144341428 },
+  { id: 9, name: "Luisa Maldonado", number: 3192837931 },
 ];
-alert("Añadir nuevo contacto");
-const nuevocontacto = (contactos.push = {
-  id: window.prompt("Ingresar contacto"),
-  nombre: window.prompt("Ingresar nombre"),
-  numero: window.prompt("añadir numero"),
+
+const newcontact = contactlist.push({
+  id: 10,
+  name: "Claudia Gonzales",
+  number: 3054167392,
 });
 
-const eliminarcontacto = contactos.shift();
-console.log(contactos);
+alert("Añadir mas datos");
+contactlist.push({
+  nombres: window.prompt(" Ingresar nombre"),
+  apellido: window.prompt(" Ingresar Apellido "),
+  telefono: window.prompt("Ingresar Numero de contacto"),
+  Ubicacion: window.prompt("Ingresar Ciudad"),
+  Localizacion: window.prompt("Ingresar Dirección"),
+});
+
+const deletecontact = contactlist.shift();
+
+console.log(contactlist);
+
+const moreinformation = contactlist.push({
+  id: 11,
+  nombres: "Nombres",
+  apellido: "Apellidos",
+  telefono: "Numero de contacto",
+  ubicación: { ciudad: "Ciudad", dirección: "Direccion" },
+});
